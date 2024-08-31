98200c96c574536ef5f3526918886f052b985f93
NeveSGG <neverovsg@gmail.com>
2024-08-31 12:11:12 +0600

    feat: save

--- src/LevelsManager/LevelsManager.ts
+++ src/LevelsManager/LevelsManager.ts
@@ -1,53 +1,27 @@
+import { IRawThing } from "../LevelMap/blocks/Thing/interface";
 import { LevelMap } from "../LevelMap/LevelMap";
+import levelsData from "./levels.json";
 
 class LevelsManager {
-    levels: { [id: number]: LevelMap } = {};
+  levels: { [id: number]: LevelMap } = {};
 
-    createLevel(id: number) {
-        this.levels[id] = new LevelMap(id, [
-            {'x':4,'y':4,color:'#FFFF00'},
-            {'x':4,'y':6,color:'#FFFF00'},
-            {'x':2,'y':2,color:'#FF0000'},
-            {'x':3,'y':2,color:'#00FF00'},
-            {'x':2,'y':3,color:'#00FF00'},
-            {'x':3,'y':3,color:'#0000FF'},
-            {'x':6,'y':3,color:'#FFFF00'},
-            {'x':2,'y':4,color:'#FF00FF'},
-            {'x':3,'y':4,color:'#00FFFF'},
-            {'x':5,'y':4,color:'#00FFFF'},
-            {'x':6,'y':4,color:'#800080'},
-            {'x':3,'y':5,color:'#FFFF00'},
-            {'x':4,'y':5,color:'#FFA500'},
-            {'x':5,'y':5,color:'#800080'},
-            {'x':6,'y':5,color:'#FF0000'},
-            {'x':3,'y':6,color:'#0000FF'},
-            {'x':5,'y':6,color:'#FFA500'},
-            {'x':6,'y':6,color:'#FF00FF'}
-        ])
-    }
+  importLevels() {
+    levelsData.forEach((levelData, ind) => {
+      this.createLevel(ind, levelData);
+    });
+  }
 
-    resetLevel(id: number) {
-        this.levels[id] = new LevelMap(id, [
-            {'x':4,'y':4,color:'#FFFF00'},
-            {'x':4,'y':6,color:'#FFFF00'},
-            {'x':2,'y':2,color:'#FF0000'},
-            {'x':3,'y':2,color:'#00FF00'},
-            {'x':2,'y':3,color:'#00FF00'},
-            {'x':3,'y':3,color:'#0000FF'},
-            {'x':6,'y':3,color:'#FFFF00'},
-            {'x':2,'y':4,color:'#FF00FF'},
-            {'x':3,'y':4,color:'#00FFFF'},
-            {'x':5,'y':4,color:'#00FFFF'},
-            {'x':6,'y':4,color:'#800080'},
-            {'x':3,'y':5,color:'#FFFF00'},
-            {'x':4,'y':5,color:'#FFA500'},
-            {'x':5,'y':5,color:'#800080'},
-            {'x':6,'y':5,color:'#FF0000'},
-            {'x':3,'y':6,color:'#0000FF'},
-            {'x':5,'y':6,color:'#FFA500'},
-            {'x':6,'y':6,color:'#FF00FF'}
-        ])
+  createLevel(id: number, levelData: IRawThing[]) {
+    this.levels[id] = new LevelMap(id, levelData);
+  }
+
+  resetLevel(id: number) {
+    const levelData: IRawThing[] | undefined = levelsData[id];
+
+    if (levelData) {
+      this.createLevel(id, levelData);
     }
+  }
 }
 
 export const levelsManager = new LevelsManager();
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { levelsManager } from "./LevelsManager/LevelsManager";
 import { globalState } from "./GlobalState/GlobalState";
 
-levelsManager.createLevel(0);
-levelsManager.createLevel(1);
-
+levelsManager.importLevels();
 globalState.setMenuStage();
